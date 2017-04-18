324cdfb72078edcf77c8e82165d6c22e218c9b18
Mathieu Lapeyre <mathieu.lapeyre@gatech.edu>
2017-04-18 07:40:33 +0200

    First pre-compiled template + partial

--- compiled.js
+++ compiled.js
@@ -1,13 +1,24 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['docIntroduction'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, alias1=container.lambda, alias2=container.escapeExpression;
+
+  return "<h1>"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.info : depth0)) != null ? stack1.name : stack1), depth0))
+    + "</h1>\n<p>"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.info : depth0)) != null ? stack1.description : stack1), depth0))
+    + "</p>\n<p>"
+    + alias2((helpers.filter || (depth0 && depth0.filter) || helpers.helperMissing).call(depth0 != null ? depth0 : {},((stack1 = (depth0 != null ? depth0.info : depth0)) != null ? stack1.description : stack1),"markdown",{"name":"filter","hash":{},"data":data}))
+    + "</p>";
+},"useData":true});
 templates['itemMenu'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper;
 
-  return "     <div class=\"item\">\n         "
+  return "\n         <div class=\"title\">\n             <i class=\"dropdown icon\"></i>\n             <b>"
     + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
-    + "\n"
-    + ((stack1 = container.invokePartial(partials.requestMenu,depth0,{"name":"requestMenu","data":data,"indent":"         ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + "     </div>\n";
+    + "</b>\n         </div>\n         <div class=\"content menu\">\n"
+    + ((stack1 = container.invokePartial(partials.requestMenu,depth0,{"name":"requestMenu","data":data,"indent":"             ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "         </div>\n\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
--- src/js/docIntro.js
+++ src/js/docIntro.js
@@ -1,12 +1,15 @@
-var tpl_itemMenu = Handlebars.templates['itemMenu'];
+var tpl_docIntro = Handlebars.templates['docIntroduction'];
 Handlebars.partials = Handlebars.templates;
 
+var Filter = require("handlebars.filter");
+
 //var data = {name: "Luke", power: "force"};
+Filter.registerHelper(Handlebars);
 
 $.getJSON("http://127.0.0.1:8080/src/assets/FusioAPI_Doc.postman_collection.json", function(data) {
   //Render the data into the template
-  var rendered = tpl_itemMenu(data);
+  var rendered = tpl_docIntro(data);
 
 //Overwrite the contents of #target with the renderer HTML
-  document.getElementById('itemMenu').innerHTML = rendered;
+  document.getElementById('docIntroduction').innerHTML = rendered;
 });
